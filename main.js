789abe5a6295a2bac8e8818020e80c25446e1da8
olumide osuntuyi <olugeorge61@gmail.com>
2024-09-06 13:52:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const chessboard = document.getElementById('chessboard');
+const pieces = {
+    'r': '♜', 'n': '♞', 'b': '♝', 'q': '♛', 'k': '♚', 'p': '♟',
+    'R': '♖', 'N': '♘', 'B': '♗', 'Q': '♕', 'K': '♔', 'P': '♙'
+};
+
+const initialBoard = [
+    'rnbqkbnr',
+    'pppppppp',
+    '........',
+    '........',
+    '........',
+    '........',
+    'PPPPPPPP',
+    'RNBQKBNR'
+];
+
+function createBoard() {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            const square = document.createElement('div');
+            square.className = 'square ' + ((i + j) % 2 === 0 ? 'white' : 'black');
+            square.dataset.row = i;
+            square.dataset.col = j;
+            if (initialBoard[i][j] !== '.') {
+                square.textContent = pieces[initialBoard[i][j]];
+            }
+            chessboard.appendChild(square);
+        }
+    }
+}
+
+createBoard();
